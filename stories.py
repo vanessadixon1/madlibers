68ea5cf1b572e599d1f31af38e43f1ad768601f9
Vanessa Dixon <53667448+vanessadixon1@users.noreply.github.com>
2023-10-25 12:13:24 -0400

    Update stories.py

--- stories.py
+++ stories.py
@@ -35,9 +35,6 @@ class Story:
         return text
 
 
-# Here's a story to get you started
-
-
 story = Story(
     ["place", "noun", "verb", "adjective", "plural_noun"],
     """Once upon a time in a long-ago {place}, there lived a
